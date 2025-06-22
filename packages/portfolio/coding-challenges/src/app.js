cc87f46d314abb0385d2f6bb01ab6c5ac11a945c
Grace Akpan <opps4lrng@gmail.com>
2025-06-21 23:53:38 -0600

    updated text

--- packages/portfolio/coding-challenges/src/app.js
+++ packages/portfolio/coding-challenges/src/app.js
@@ -6,7 +6,7 @@ export default function ChallengesPage() {
       <h2>Coding Challenges</h2>
       <p>
         Note: Build a UI for each coding challenge. Solve and code each
-        challenge. For example: palindrome, fizzbuzz, etc.{" "}
+        challenge. For example: palindrome, fizzbuzz, etc.
       </p>
     </div>
   );
